3f86a6e7b899d8934fe5d91bd7a67327dbd7cac3
aprskalo1 <ante.prskalo4@gmail.com>
2024-06-11 21:35:21 +0200

    categories crud done

--- src/main/java/hr/shop/apartmentsshop/controller/mvc/CategoryController.java
+++ src/main/java/hr/shop/apartmentsshop/controller/mvc/CategoryController.java
@@ -7,7 +7,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping
@@ -20,5 +22,36 @@ public class CategoryController {
         model.addAttribute("categories", categoryService.getCategories());
         model.addAttribute("category", new Category());
         return "categories";
+    }
+
+    @PostMapping("/saveCategory")
+    public String saveCategory(Model model, Category category, RedirectAttributes redirectAttrs) {
+        try {
+            categoryService.saveCategory(category);
+        } catch (Exception e) {
+            redirectAttrs.addFlashAttribute("errorMessage", e.getMessage());
+        }
+        return "redirect:/categories";
+    }
+
+    @PostMapping("/updateCategory")
+    public String updateCategory(Model model, Category category, RedirectAttributes redirectAttrs) {
+        try {
+            Category existingCategory = categoryService.getCategories().stream().filter(c -> c.getId().equals(category.getId())).findFirst().orElse(null);
+            if (existingCategory == null) {
+                throw new Exception("Category not found");
+            }
+            existingCategory.setName(category.getName());
+            categoryService.updateCategory(existingCategory);
+        } catch (Exception e) {
+            redirectAttrs.addFlashAttribute("errorMessage", e.getMessage());
+        }
+        return "redirect:/categories";
+    }
+
+    @PostMapping("/deleteMovieGenre")
+    public String deleteMovieGenre(Model model, Category movieGenre) {
+        categoryService.deleteCategory(movieGenre.getId());
+        return "redirect:/categories";
     }
 }
